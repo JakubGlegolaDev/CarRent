927f45d966d9a2ce8ba6b8f9bce1c70862080886
Kuba Glegoła <jglegola6@gmail.com>
2017-12-03 22:09:09 +0100

    ...

--- src/databaseOperations/DatabaseOperationTest.java
+++ src/databaseOperations/DatabaseOperationTest.java
@@ -11,9 +11,7 @@ public static void main(String args[]) throws Exception {
 		dboper.getConnection();
 		dboper.getALL();
 		dboper.getID();
-		dboper.checkExistance();
-		dboper.insertExample("name");
-		dboper.insertExample("imie2");
+		dboper.checkExistanceCar();
 		
 //		dboper.inserExamplePS("Malgorzata");
 		
--- src/databaseOperations/DatabaseOperations.java
+++ src/databaseOperations/DatabaseOperations.java
@@ -53,6 +53,20 @@ public class DatabaseOperations {
 		return null;
 	}
 	
+	public ArrayList<String> getStatus() throws Exception{
+		
+		String selectStatus = "SELECT model, make, year, availability FROM Cars WHERE availability='Yes';";
+		preparedStatement = connection.prepareStatement(selectStatus);
+		
+		
+		try {
+			ResultSet result = preparedStatement.executeQuery(selectStatus);
+		} catch(Exception e){
+			System.out.println(e);
+		}
+		return null;
+	}
+	
 	
 	public ArrayList<String> getID() throws Exception{
 		
@@ -74,12 +88,12 @@ public class DatabaseOperations {
 	}
 	
 	
-	public void checkExistance() throws Exception{
+	public void checkExistanceClient() throws Exception{
 		
 		try {
 	
 			dbMetaData = connection.getMetaData();
-			ResultSet result = dbMetaData.getTables(null, null, "testowa", null);
+			ResultSet result = dbMetaData.getTables(null, null, "Client", null);
 			if(result.next()) {
 				System.out.println("Base found");
 			} else {
@@ -89,6 +103,56 @@ public class DatabaseOperations {
 			System.out.println(e);
 		}
 	}
+	
+	public void checkExistanceEmployee() throws Exception{
+			
+			try {
+		
+				dbMetaData = connection.getMetaData();
+				ResultSet result = dbMetaData.getTables(null, null, "Emplyoee", null);
+				if(result.next()) {
+					System.out.println("Base found");
+				} else {
+					System.out.println("Base does not found");
+				}
+			}catch(Exception e){
+				System.out.println(e);
+			}
+		}
+	
+	public void checkExistanceHire() throws Exception{
+			
+			try {
+		
+				dbMetaData = connection.getMetaData();
+				ResultSet result = dbMetaData.getTables(null, null, "Hire", null);
+				if(result.next()) {
+					System.out.println("Base found");
+				} else {
+					System.out.println("Base does not found");
+				}
+			}catch(Exception e){
+				System.out.println(e);
+			}
+		}
+	
+	public void checkExistanceCar() throws Exception{
+			
+			try {
+		
+				dbMetaData = connection.getMetaData();
+				ResultSet result = dbMetaData.getTables(null, null, "Car", null);
+				if(result.next()) {
+					System.out.println("Base found");
+				} else {
+					System.out.println("Base does not found");
+				}
+			}catch(Exception e){
+				System.out.println(e);
+			}
+		}
+
+
 	
 	
 	public void insertExample(String name) throws Exception {
@@ -126,6 +190,7 @@ public class DatabaseOperations {
 		preparedStatement.setString(2, make);
 		preparedStatement.setInt(3, year);
 		preparedStatement.setString(4, availability);
+		
 		
 		try {
 			preparedStatement.executeUpdate();
